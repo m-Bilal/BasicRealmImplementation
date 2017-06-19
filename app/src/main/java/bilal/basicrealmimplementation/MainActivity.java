ba6cb6fe6db05ba159e738219068cde6c1ec8ca9
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-19 13:45:27 +0530

    Created realm configuration

--- app/src/main/java/bilal/basicrealmimplementation/MainActivity.java
+++ app/src/main/java/bilal/basicrealmimplementation/MainActivity.java
@@ -3,11 +3,33 @@ package bilal.basicrealmimplementation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
+
+    Realm realm;
+    RealmConfiguration config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createRealmConfig();
+        realm = Realm.getInstance(config);
+    }
+
+    void createRealmConfig() {
+        // initialize Realm
+        Realm.init(getApplicationContext());
+
+        // create your Realm configuration
+        config = new RealmConfiguration
+                .Builder()
+                .deleteRealmIfMigrationNeeded()
+                .name("myCustomRealm")
+                .build();
+        Realm.setDefaultConfiguration(config);
     }
 }
